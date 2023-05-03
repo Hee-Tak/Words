5f88140b89d462c82bca51a685b3d26cbad8075d
hitak77 <hitak77@naver.com>
2023-05-04 03:39:24 +0900

    아 이건 좀 아니잖아.. 진짜 열심히했는데... 왜 솔루션코드에서는 아예 다른얘기하고있는건데...

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -16,8 +16,10 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -38,8 +40,11 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = binding.recyclerView
         // Sets the LinearLayoutManager of the recyclerview
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = LetterAdapter()
+        // recyclerView.layoutManager = LinearLayoutManager(this)
+        // recyclerView.adapter = LetterAdapter()
+
+        // Sets the LinearLayoutManager of the recylerview
+        chooseLayout()
 
 
     }
@@ -57,8 +62,60 @@ class MainActivity : AppCompatActivity() {
         if(menuItem == null)
             return
 
+        // Set the drawable for the menu icon based on which LayoutManager is currently in use
+
+        // An if-clause can be used on the right side of an assignment if all paths return a value.
+        // The following code is equivalent to
+        // if (isLinearLayoutManager)
+        //      menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        // else menu.icon = ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+
+        menuItem.icon =
+            if(isLinearLayoutManager)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+
+        // menuItem.icon = ContextCompat.getDrawable(this, if(isLinearLayoutManager) {R.drawable.ic_grid_layout} else {R.drawable.ic_linear_layout} )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)  //메뉴 객체 생성
+        //inflate : xml 레이아웃 파일을 코드에서 사용할 수 있는 뷰 객체 계층 구조로 확장하는 과정을 말함
+        // 즉, 메뉴 xml 파일 (layout_menu.xml)의 레이아웃을 가져와서 메뉴를 구성하는 뷰 객체 계층 구조를 생성하는 역할
+        // 이렇게 생성된 뷰 객체 계층 구조는 코드에서 직접적으로 조작 가능
+        // onCreateOptionMenu 메소드에서 주로 사용되는 거. 메뉴를 생성할 때 사용됨
+
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        // Calls code to set the icon based on the LinearLayoutManager of the RecyclerView
+        setIcon(layoutButton)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+
+        return when(item.itemId) {
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager (a Boolean) to the opposite(상반되는 반대되는) value
+                isLinearLayoutManager = !isLinearLayoutManager
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+            // Otherwise, do nothing and use the core event handling (핵심 이벤트 처리)
+
+            // when clauses(when절) require that all possible paths be accounted for explicitly, (명시적인)
+            //   for instance both the true and false cases if the value is a Boolean,
+            //   or an else to catch all unhandled cases.
+            else -> super.onOptionsItemSelected(item)
+        }
 
     }
+
+
 }
 
 /**
